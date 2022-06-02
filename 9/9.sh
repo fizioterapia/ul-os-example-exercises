d7b54ac04ccf87210c0f602a0ea6cedafeff818f
fizioterapia <cubasa4@gmail.com>
2022-06-02 20:57:53 +0200

    finish exercise 9

--- 9/9.sh
+++ 9/9.sh
@@ -9,3 +9,13 @@
 # Active connections:
 # **********
 # 10 stars equals 10 connections
+
+while true; do
+    STARS=`netstat -apt 2>/dev/null | grep ESTABLISHED | wc -l`
+
+    echo -ne "\r                                                                           \r"
+    for i in `seq 1 $STARS`; do
+        echo -ne "*"
+    done
+    sleep 5
+done
