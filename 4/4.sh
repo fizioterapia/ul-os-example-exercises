c24a4dbd9a606c8c9941cbce494c066442f5fafc
fizioterapia <cubasa4@gmail.com>
2022-06-02 19:17:39 +0200

    finish exercise 4

--- 4/4.sh
+++ 4/4.sh
@@ -2,3 +2,11 @@
 
 # Exercise 4
 # Check if string is palindrome.
+
+STRING=`echo $@ | tr "[:upper:]" "[:lower:]" | tr -d "[:blank:]" | tr -d "[:punct:]"`
+REVSTRING=`echo $STRING | rev`
+if [ $STRING == $REVSTRING ]; then
+    echo "$@ is palindrome"
+else
+    echo "$@ is not palindrome"
+fi
