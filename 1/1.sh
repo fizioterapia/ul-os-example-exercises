0faa4b98a22b628ad4e3c874925a403a7f096b76
fizioterapia <cubasa4@gmail.com>
2022-06-02 18:43:44 +0200

    finished exercise 1

--- 1/1.sh
+++ 1/1.sh
@@ -3,3 +3,14 @@
 # Exercise 1
 # Print on the screen all users that are currently logged in.
 # Format: LOGIN --- NAME --- UID --- (SESSIONS)
+
+IFS=$'\n' # Input Field Separator (https://bash.cyberciti.biz/guide/$IFS)
+USERS=`who | tr -s ' ' | cut -d' ' -f1 | uniq -c`
+for USER in $USERS; do
+    SESSIONS=`echo $USER | tr -s ' ' | cut -d' ' -f2`
+    LOGIN=`echo $USER | tr -s ' ' | cut -d' ' -f3`
+    DATA=`cat /etc/passwd | grep ^$LOGIN`
+    NAME=`echo $DATA | cut -d':' -f5`
+    UUID=`echo $DATA | cut -d':' -f3`
+    echo "$LOGIN --- $NAME --- $UUID --- $SESSIONS";
+done
