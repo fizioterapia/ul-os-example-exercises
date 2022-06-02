6a71a06477dc01ec5625316df13580ebd215cbca
fizioterapia <cubasa4@gmail.com>
2022-06-02 20:54:47 +0200

    finish exercise 8

--- 8/8.sh
+++ 8/8.sh
@@ -2,7 +2,28 @@
 
 # Exercise 8
 # Create file etc.all which will be combination
-# of all files that you can read in /etc directory
+# of 20 lines of all files that you can read in /etc directory
 # before appending etc.all include line with
 # format: /* --- file name --- */
 # in the end include number of files you readed
+
+ETC_FILES=`find /etc -readable -type f 2>/dev/null`
+OUTPUT="etc.all"
+
+IRS=$'\n'
+FILES_READ=0
+
+echo "" > $OUTPUT
+
+
+for FILE in $ETC_FILES; do
+    if [ -r $FILE ]; then
+        FILE_CONTENT=`head -n 20 $FILE`
+        echo "/* --- $FILE --- */" >> $OUTPUT
+        echo $FILE_CONTENT >> $OUTPUT
+
+        FILES_READ=`expr $FILES_READ + 1`
+    fi
+done
+
+echo "Read $FILES_READ files." >> $OUTPUT
