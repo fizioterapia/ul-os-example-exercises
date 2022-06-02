7b65f703530d6185289b6436156fee7b549b5e61
fizioterapia <cubasa4@gmail.com>
2022-06-02 20:29:59 +0200

    finished exercise 6

--- 6/6.sh
+++ 6/6.sh
@@ -9,4 +9,42 @@
 # ---
 # $HOUR
 # ---
-# [+] UID or [-] UID
+# uid
+
+LAST_WHO="who.txt"
+LIST="list.txt"
+
+while true; do
+    IRS=$'\n'
+    NEW_USERS=`who | tr -s " " | cut -d" " -f1 | uniq`;
+
+    echo "---" >> $LIST
+    echo "$(date +%H:%M:%S)" >> $LIST
+    echo "---" >> $LIST
+
+    for USER in `cat $LAST_WHO`; do
+        OUTPUT=`cat $LAST_WHO | grep $USER`
+        LENGTH=${#OUTPUT} # length of string
+        NEWOUTPUT=`echo $NEW_USERS | grep $USER`
+        NEWLENGTH=${#NEWOUTPUT}
+
+        if [[ ($LENGTH -gt 0 && $NEWLENGTH -eq 0) ]]; then
+            echo `id $USER | cut -d'(' -f1 | cut -d'=' -f2` >> $LIST
+        fi
+    done
+
+    for USER in $NEW_USERS; do
+        OUTPUT=`cat $LAST_WHO | grep $USER`
+        LENGTH=${#OUTPUT} # length of string
+        NEWOUTPUT=`echo $NEW_USERS | grep $USER`
+        NEWLENGTH=${#NEWOUTPUT}
+
+        if [[ ($LENGTH -eq 0 && $NEWLENGTH -gt 0) ]]; then
+            echo `id $USER | cut -d'(' -f1 | cut -d'=' -f2` >> $LIST
+        fi
+    done
+
+    echo "$NEW_USERS" > $LAST_WHO
+
+    sleep 5
+done
