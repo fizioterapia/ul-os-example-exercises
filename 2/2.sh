69210da5765a3b1e3f79c7f0bb27b590abccc599
fizioterapia <cubasa4@gmail.com>
2022-06-02 18:52:04 +0200

    finish exercise 2

--- 2/2.sh
+++ 2/2.sh
@@ -5,3 +5,19 @@
 # L < K < P
 # If K equals to existing UID, then don't print it.
 # You're only inputing 2 variables, L and P.
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <from> <to>"
+    exit 1
+fi
+
+L=$1
+P=$2
+
+for K in `seq $1 $2`; do
+    OUTPUT=`cat /etc/passwd | grep ".*:.*:$K"`
+    LENGTH=${#OUTPUT} # length of string
+    if [ $LENGTH -eq 0 ]; then
+        echo $K;
+    fi
+done
